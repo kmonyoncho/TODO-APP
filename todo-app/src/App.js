a06f3dab0145bc6b78090f928a71b6faac9744f0
kmonyoncho <kepha.manwa@outlook.com>
2021-06-07 14:26:08 +0300

    todo function button

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -4,11 +4,18 @@ import './App.css';
 function App() {
   const [todos,setTodos]=useState(['Take dogs for a walk','Take the rubish out','Welcome to funsoft systems']);
   const[input,setInput]=useState('');
+  console.log(input);
+
+  const addTodo = (event) =>{
+    //this will fire when the button is clicked
+    console.log('The button clicked');
+    setTodos([...todos,input]);
+  }
   return (
     <div className="App">
       <h1>Hello World</h1>
       <input value={input} onChange={event=> setInput(event.target.value)}/>
-      <button>Add Todo</button>
+      <button onClick={addTodo}>Add Todo</button>
       <ul>
         {todos.map(todo =>(
         <li>{todo}</li>
