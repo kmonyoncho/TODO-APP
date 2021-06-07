b7f4fc04ae89967172dde65c4553bdb930e392c5
kmonyoncho <kepha.manwa@outlook.com>
2021-06-07 14:14:44 +0300

    input

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -2,16 +2,17 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [todos,setTodos]=useState(['Take dogs for a walk','Take the rubish out']);
+  const [todos,setTodos]=useState(['Take dogs for a walk','Take the rubish out','Welcome to funsoft systems']);
+  const[input,setInput]=useState('');
   return (
     <div className="App">
       <h1>Hello World</h1>
-      <input />
+      <input value={input} onChange={event=> setInput(event.target.value)}/>
       <button>Add Todo</button>
-
       <ul>
         {todos.map(todo =>(
-        <li>{todo}</li>))}
+        <li>{todo}</li>
+        ))}
         
       </ul>
 
