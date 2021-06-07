d6caa52f8c8054e839ec31338861ad4416c8d43b
kmonyoncho <kepha.manwa@outlook.com>
2021-06-07 09:21:48 +0300

    foundation

---
+++ todo-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <input />
+      <button>Add Todo</button>
+
+      <ul>
+        <li>Take dogs for a walk</li>
+        <li>Take the rubish out</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
